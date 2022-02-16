30e4f1ca115c1ae2c29623b8b9e3c7336b470e47
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-02-16 14:48:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
 
-a1, a2, a3 = st.columns(4)
+a1, a2, a3, a4 = st.columns(4)
 a1.metric("Temperature", "70 °F", "1.2 °F")
 a2.metric("Wind", "9 mph", "-8%")
 a3.metric("Humidity", "86%", "4%")
