7d6ac4db592caace9f9fe3951eab56f30f7301a3
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-02-16 20:09:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,15 @@ st.set_page_config(layout="wide")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
-col1, col2, col3 = st.columns(3)
+a1, a2, a3 = st.columns(3)
 
-col1.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
-
-a1, a2, a3, a4 = st.columns(4)
-with a1:
-    with st.container():
-        st.metric("Temperature", "70 °F", "1.2 °F")
-    with st.container():
-        st.bar_chart(np.random.randn(5, 3))
+a1.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
 a2.metric("Wind", "9 mph", "-8%")
 a3.metric("Humidity", "86%", "4%")
-a4.metric("Humidity", "86%", "4%")
+
+b1, b2, b3, b4 = st.columns(4)
+with b1:
+    st.metric("Temperature", "70 °F", "1.2 °F")
+b2.metric("Wind", "9 mph", "-8%")
+b3.metric("Humidity", "86%", "4%")
+b4.metric("Humidity", "86%", "4%")
