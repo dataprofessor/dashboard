c0a1c157505e7a31090124031ef4c1652999af63
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-02-16 20:03:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,15 @@ with open('style.css') as f:
 col1, col2, col3 = st.columns(3)
 
 col1.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
-#col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
 
 a1, a2, a3, a4 = st.columns(4)
 with a1:
-    st.metric("Temperature", "70 °F", "1.2 °F")
-    st.metric("Temperature", "70 °F", "1.2 °F")
+    with st.container():
+        st.metric("Temperature", "70 °F", "1.2 °F")
+    with st.container():
+        st.metric("Temperature", "70 °F", "1.2 °F")
 a2.metric("Wind", "9 mph", "-8%")
 a3.metric("Humidity", "86%", "4%")
 a4.metric("Humidity", "86%", "4%")
