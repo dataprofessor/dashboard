f49326976e9c7db4bbde6f6070de3bc73eea07fd
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-02-17 17:17:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ b2.metric("Wind", "9 mph", "-8%")
 b3.metric("Humidity", "86%", "4%")
 b4.metric("Humidity", "86%", "4%")
 
-c1, c2 = st.columns((2,1))
+c1, c2 = st.columns((3,1))
 with c1:
     st.markdown('### Heatmap')
     plost.time_hist(
