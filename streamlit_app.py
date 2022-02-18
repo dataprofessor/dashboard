a1cedb2ebe5706471b64af428cc5236a03377f72
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-02-18 09:06:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,30 +2,32 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plost
-
 from PIL import Image
 
+# Page setting
 st.set_page_config(layout="wide")
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+# Data
 seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
 stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
 
+# Row A
 a1, a2, a3 = st.columns(3)
-
 a1.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
 a2.metric("Wind", "9 mph", "-8%")
 a3.metric("Humidity", "86%", "4%")
 
+# Row B
 b1, b2, b3, b4 = st.columns(4)
-with b1:
-    st.metric("Temperature", "70 °F", "1.2 °F")
+b1.metric("Temperature", "70 °F", "1.2 °F")
 b2.metric("Wind", "9 mph", "-8%")
 b3.metric("Humidity", "86%", "4%")
 b4.metric("Humidity", "86%", "4%")
 
+# Row C
 c1, c2 = st.columns((7,3))
 with c1:
     st.markdown('### Heatmap')
@@ -36,8 +38,7 @@ with c1:
     y_unit='day',
     color='temp_max',
     aggregate='median',
-    legend=None,
-)
+    legend=None)
 with c2:
     st.markdown('### Bar chart')
     plost.donut_chart(
